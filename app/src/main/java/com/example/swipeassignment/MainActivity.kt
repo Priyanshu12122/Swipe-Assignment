ba71b4b29135343b895c8e26ccf51abb6dadafb0
priyanshu kumar <priyanshu7595934436@gmail.com>
2025-01-06 19:35:05 +0530

    Initial

--- app/src/main/java/com/example/swipeassignment/MainActivity.kt
+++ app/src/main/java/com/example/swipeassignment/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.example.swipeassignment.navigation.NavGraph
 import com.example.swipeassignment.ui.theme.SwipeAssignmentTheme
@@ -11,6 +12,7 @@ import com.example.swipeassignment.ui.theme.SwipeAssignmentTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent {
